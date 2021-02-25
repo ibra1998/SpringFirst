f05438919611b4646e8081e845f2dd4643c702bf
zakar <zakar@192.168.1.146>
2021-02-25 09:57:49 +0100

    comit inicial

---
+++ src/test/java/com/example/maven2/Maven2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.maven2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Maven2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
